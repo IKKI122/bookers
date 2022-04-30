06c366dce6c0bcfbfbe800d7263cfcb29acf59bc
IKKI122 <100700431+IKKI122@users.noreply.github.com>
2022-04-30 13:59:21 +0000

    [Add]更新・削除機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,5 @@
 class BooksController < ApplicationController
   def new
-    @book=Book.new
   end
   
   def create
@@ -26,6 +25,12 @@ class BooksController < ApplicationController
     book=Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to books_path(book.id)
   end
   
   private
